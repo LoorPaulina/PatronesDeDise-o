7880844e1a147ca5bbd184c6a1d54c1b3ff0ce04
LoorPaulina <paumloor@espol.edu.ec>
2022-12-01 18:51:17 -0500

    Actualizacion factory method

--- Tienda/src/Tarjetas/Asignaciones.java
+++ Tienda/src/Tarjetas/Asignaciones.java
@@ -1,20 +1,12 @@
 package Tarjetas;
 
 public class Asignaciones {
-	private TarjetaDeCredito tarjeta;
+
 	
-	private void asignarTarjetaCliente(Cliente cliente) {
-		if((cliente.getTiempoCreacionCuentaBancaria()>3) && (cliente.getIngresosMensuales()>10000)&& (cliente.getExperienciaCrediticia()>20)) {
-			tarjeta=new TarjetaVIP();
-			cliente.setTarjeta(tarjeta);	
-		}
-		
-		if((cliente.getTiempoCreacionCuentaBancaria()<3) &&(cliente.getIngresosMensuales()<10000)) {
-			tarjeta=new TarjetaPremium();
-			cliente.setTarjeta(tarjeta);
-		}else{
-			tarjeta=new TarjetaBasica();
-			cliente.setTarjeta(tarjeta);
-		}
+	private void asignarTarjeta(Cliente cliente) {
+		TarjetaDeCreditoFactory factory= new TarjetaDeCreditoFactory();
+		cliente.setTarjeta(factory.createTarjeta(cliente));
 	}
 }
+
+
--- Tienda/src/Tarjetas/TarjetaDeCreditoFactory.java
+++ Tienda/src/Tarjetas/TarjetaDeCreditoFactory.java
@@ -1,5 +1,22 @@
 package Tarjetas;
 
-public abstract class TarjetaDeCreditoFactory {
-	abstract TarjetaDeCredito createTarjetaDeCredito();
+public class TarjetaDeCreditoFactory {
+	private TarjetaDeCredito tarjeta;
+	
+	public TarjetaDeCredito createTarjeta(Cliente cliente) {
+		if((cliente.getTiempoCreacionCuentaBancaria()>3) && (cliente.getIngresosMensuales()>10000)&& (cliente.getExperienciaCrediticia()>20)) {
+			tarjeta=new TarjetaVIP();
+			return tarjeta;	
+		}
+		
+		if((cliente.getTiempoCreacionCuentaBancaria()<3) &&(cliente.getIngresosMensuales()<10000)) {
+			tarjeta=new TarjetaPremium();
+			return tarjeta;
+		}else{
+			tarjeta=new TarjetaBasica();
+			return tarjeta;
+		}
+	}
 }
+
+
